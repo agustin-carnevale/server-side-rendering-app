5631f9899243c8431e7e9dad9bc1786f24af6e7a
Agustin Carnevale <agustin@Agustins-MacBook-Air.local>
2018-10-25 20:15:48 -0300

    SEO meta-tags, react helmet

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -5,6 +5,7 @@ import {Provider} from 'react-redux';
 import {renderRoutes} from 'react-router-config';
 import serialize from 'serialize-javascript';
 import Routes from '../client/Routes';
+import {Helmet} from 'react-helmet';
 
 
 export default (req,store,context) =>{
@@ -16,10 +17,14 @@ export default (req,store,context) =>{
 				</StaticRouter>
 			</Provider>
 		);
+	
+	const helmet = Helmet.renderStatic();
 
 	return `
 		<html>
 			<head>
+				${helmet.title.toString()}
+				${helmet.meta.toString()}
 				<!-- Compiled and minified CSS -->
 				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 			</head>
